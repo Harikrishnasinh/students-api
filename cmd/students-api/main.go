f76d2e6d7813fddb84f736d5fce6c34dfd5d734c
Harikrishnasinh <harikrishnasinh16@gmail.com>
2026-01-02 17:21:00 +0530

    initialize main application and module configuration

---
+++ cmd/students-api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Entry point for the students-api application
+	fmt.Println("Hello welcome students!!!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Harikrishnasinh/go-students-api
+
+go 1.25.5
