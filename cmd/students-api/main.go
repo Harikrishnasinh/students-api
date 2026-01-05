5284a3e44e5eab0c93d812d31064fef68e5b0033
Harikrishnasinh <harikrishnasinh16@gmail.com>
2026-01-05 17:52:12 +0530

    refactor: enhance logging and implement graceful shutdown for server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Harikrishnasinh/go-students-api/internal/config"
 )
@@ -11,7 +16,6 @@ func main() {
 	// Entry point for the students-api application
 	fmt.Println("Hello welcome students!!!")
 	cfg := config.MustLoad()
-	fmt.Printf("Loaded config: %+v\n", cfg)
 
 	// connection of database
 	// > currently it is in pending state
@@ -20,7 +24,7 @@ func main() {
 	routes := http.NewServeMux()
 
 	routes.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Handling root route")
+		slog.Info("Root route accessed")
 		w.Write([]byte("Welcome to Students API"))
 	})
 
@@ -30,12 +34,54 @@ func main() {
 		Handler: routes,
 	}
 
-	fmt.Printf("Server is listening on %s", cfg.HttpServer.Address)
+	slog.Info("Server is listening on", "address", cfg.HttpServer.Address)
 
-	// listening on the port (localhost:8080)
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Printf("Failed to start server: %s\n", err.Error())
+	// graceful shutdown, making done channel for notifying the shutdown signal i.e interrupt
+
+	// Flow of Execution (Step by Step)
+	// 1️⃣ Program starts
+	// done channel is created
+	// OS signal listener is registered
+
+	// 2️⃣ Server starts
+	// Server runs in a separate goroutine
+	// Main goroutine continues
+
+	// 3️⃣ Main goroutine waits
+	// Execution stops at:
+	// <-done
+
+	// Program is idle but still running
+
+	// 4️⃣ User presses Ctrl + C
+	// OS sends SIGINT
+	// Go delivers it to done channel
+
+	// 5️⃣ Program unblocks
+	// <-done receives the signal
+	// Main function continues
+	// Program exits (unless more code follows)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
+
+	go func() {
+		// listening on the port (localhost:8080)
+		err := server.ListenAndServe()
+		if err != nil {
+			slog.Error("Server failed to start", "error", err.Error())
+		}
+	}()
+
+	<-done
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // 5 seconds
+	defer cancel()
+	doneContext := ctx.Done()
+	slog.Info("Shutting down server... it will be down in 5 seconds")
+	<-doneContext
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", "error", err.Error())
 	}
-
+	slog.Info("Server exited properly")
 }
